a652176bd10a2779233d092f287d6798d8e6b9f7
Mwaron <marton.aron08@gmail.com>
2024-11-23 13:47:26 +0100

    overflow

--- java.js
+++ java.js
@@ -1,6 +1,8 @@
 const animateButton = document.querySelector('.loadbutton');
 const loadscreen = document.querySelector('.loadscreen');
+const mainscreen = document.querySelector('body');
 
 animateButton.addEventListener('click', () => {
   loadscreen.classList.add('loadscreenToMainscreen');
+  mainscreen.classList.add('mainscreen');
 });
