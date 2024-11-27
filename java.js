647ed7b7582478c9aa3a92e67bf30159a51b3500
Mwaron <marton.aron08@gmail.com>
2024-11-27 10:37:24 +0100

    idk

--- java.js
+++ java.js
@@ -6,3 +6,27 @@ animateButton.addEventListener('click', () => {
   loadscreen.classList.add('loadscreenToMainscreen');
   mainscreen.classList.add('mainscreen');
 });
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.style.visibility = 'hidden';
+    if (entry.isIntersecting) {
+      const animationClass = entry.target.dataset.animation;
+      entry.target.style.visibility = 'visible';
+    
+    if (animationClass) {
+      entry.target.classList.add(animationClass);
+    
+    }
+
+    observer.unobserve(entry.target);
+  }
+  });
+});
+
+
+const animatedElements = document.querySelectorAll('[data-animation]');
+animatedElements.forEach((element) => {
+  observer.observe(element);
+})
